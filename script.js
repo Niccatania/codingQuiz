72c87d05a6772ccb0ce680070fbe7955da015170
Niccatania <98001855+Niccatania@users.noreply.github.com>
2022-03-22 20:21:10 -0700

    UPdate js
    received help from a tutor to help me move forward.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var timeEL = document.querySelector(".timer");
+var choicesEl = document.querySelector("#choices");
 var startQuestion
 var secondsLeft = 60;
 var score = ""
+var currentQuestionIndex = 0;
+
 
 
 
@@ -19,33 +22,75 @@ function setTime() {
   }, 1000); 
 }
 
+// questions variable to loop through
 
 var questions = [
   {
 questionTitle:"Q1",
-Choices:["Choice1 , Choice2,Choice3,Choice4"],
+Choices:["Choice1" , "Choice2","Choice3","Choice4"],
 Answer:"Choice1"
 }, 
 {
 questionTitle:"Q2",
-Choices:["Choice1,Choice2,Choice3,Choice4"],
+Choices:["Choice1" , "Choice2","Choice3","Choice4"],
 Answer:"Choice1"},
 {
   
   
 questionTitle:"Q3",
-Choices:["Choice1,Choice2,Choice3,Choice4"],
+Choices:["Choice1" , "Choice2","Choice3","Choice4"],
 Answer:"Choice3"
 },
 
 {
   questionTitle:"Q4",
-  Choices:["Choice1,Choice2,Choice3,Choice4"],
+  Choices:["Choice1" , "Choice2","Choice3","Choice4"],
   Answer:"Choice3"
   }
   ];
 
-// Now I need a for loop to run through this array and display the questions on the HTMl
+
+function showQuestions(){
+  var currentQuestion= questions[currentQuestionIndex];
+
+  document.querySelector("#questionTitle").textContent = currentQuestion.questionTitle;
+
+  choicesEl.innerHTML = "";
+
+  currentQuestion.Choices.forEach(function(choice){
+    var choiceBtn = document.createElement("button");
+    choiceBtn.setAttribute("class", "choice");
+    choiceBtn.setAttribute("value", choice);
+
+    choiceBtn.textContent = choice;
+
+    choicesEl.appendChild(choiceBtn);
+
+    choiceBtn.onclick = answerClick;
+
+  })
+
+}
+
+function answerClick(){
+  if (this.value !== questions[currentQuestionIndex].Answer) {
+    secondsLeft -= 10;
+  }
+
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex === questions.length) {
+    
+  } else {
+    showQuestions();
+  }
+}
+
+
+
+
+
+
 
 // On click of an answer I need the question to cycle to the next question immmediatly whether right or wrong
 
@@ -70,25 +115,26 @@ Answer:"Choice3"
 
 
 function startQuiz() {
+
+
  
-  // document.querySelector(#Quizcontainer).innerhtml
-  // pullQuestions();
+  document.querySelector("#startScreen").classList.add("hide");
+
+  document.querySelector(".questionScreen").classList.remove("hide");
+
+  
   setTime();
+  showQuestions();
   
 }
 
 
 
-function saveScore()
+// function saveScore()
 
 // Need a get item to save input of initials and score. Will be linked to a button
 
 
-function displayScore()
+// function displayScore()
 
 // Need set item for any potentially locally stored Highscores, these would load when user navigates to Highscore page
-
-
-
-
-
